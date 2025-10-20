a599918d634adf088b16455a7e90a4ac0b39f953
Mebin Mathew <mebinmathew006@gmail.com>
2025-10-20 14:19:32 +0530

    removed the frontend from repo

--- hackthon_frontend/src/api/Publicaxiosconfig.jsx
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const baseurl=import.meta.env.VITE_BASE_URL
-const publicaxiosconfig = axios.create({
-  baseURL: `${baseurl}/`,
-  withCredentials: true,
-  
-});
-export default publicaxiosconfig;
-
